73280604269533b58f7b88918b43ef8f790322ae
Juan Pablo Favale <juanpablofavale@hotmail.com>
2026-05-21 09:15:05 -0300

    completando la unidad de primer metodo

--- Metodos.cs
+++ Metodos.cs
@@ -2,7 +2,8 @@
 
 Console.WriteLine("Creación del primer método.");
 
-/*
+/* 
+--Unidad 3
 void DisplayRandomNumbers()
 {
 	Random rnd = new Random();
@@ -17,6 +18,7 @@ DisplayRandomNumbers();
 
 */
 /*
+--Unidad 4
 int[] times = {800, 1200, 1600, 2000};
 int diff = 0;
 
@@ -87,38 +89,86 @@ void AdjustTime()
 }
 */
 
-string ipv4Input = "107.31.1.5";
+/*
+//Unidad 5
+//string ipv4Input = "107.31.1.5";
+string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+string[] ip;
 
-if (ValidateLength() && ValidateZeroes() && ValidateRange()) 
+foreach (string ipv4 in ipv4Input)
 {
-    Console.WriteLine($"ip is a valid IPv4 address");
-} 
-else 
-{
-    Console.WriteLine($"ip is an invalid IPv4 address");
+	ip = ipv4.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+	if (ValidateLength() && ValidateZeroes() && ValidateRange()) 
+	{
+		Console.WriteLine($"ip {ipv4} is a valid IPv4 address");
+	} 
+	else 
+	{
+		Console.WriteLine($"ip {ipv4} is an invalid IPv4 address");
+	}
 }
 
 bool ValidateLength()
 {
-	string[] arr = ipv4Input.Split(".");
-	return arr.Length==4; 
+	return ip.Length==4; 
 }
 
 bool ValidateZeroes()
 {
-	string[] numeros = ipv4Input.Split(".");
-	bool valido = true;
-	
-	foreach (string numero in arr)
+
+	foreach (string num in ip)
 	{
-		if (numero.Length > 1 && numero.StartsWith("0"))
+		if (num.Length > 1 && num.StartsWith("0"))
 		{
-			valido = false;
+			return false;
 		}
 	}
+	
+	return true;
 }
 
 bool ValidateRange()
 {
+	
+	foreach (string num in ip)
+	{
+		//int valor;
+		int.TryParse(num, out int valor);
+		if ( valor < 0 || valor > 255)
+		{
+			return false;
+		}
+	}
+	
 	return true;
 }
+*/
+
+//Unidad 6
+Random random = new Random();
+
+string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
+string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
+string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
+string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
+
+int luck = random.Next(100);
+TellFortune();
+
+luck = random.Next(100);
+TellFortune();
+
+
+void TellFortune()
+{
+	
+	Console.WriteLine("A fortune teller whispers the following words:");
+	string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+	for (int i = 0; i < 4; i++) 
+	{
+		Console.Write($"{text[i]} {fortune[i]} ");
+	}
+	Console.WriteLine("\n");
+}
+
